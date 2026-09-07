de81111e0a8279fe3c582eca0daca6accc975136
sooraj--98 <87260584+sooraja98@users.noreply.github.com>
2026-09-08 00:38:35 +0530

    Add: buy-and-sell-crypto - submission-0

---
+++ Data Structures & Algorithms/buy-and-sell-crypto/submission-0.java
@@ -0,0 +1,12 @@
+class Solution {
+    public int maxProfit(int[] prices) {
+        int res=0;
+        for(int i=0;i<prices.length;i++){
+            for(int j=i+1;j<prices.length;j++){
+                res=Math.max(res,prices[j]-prices[i]);
+            }
+        }
+
+        return res;
+    }
+}
